ad052be2ff41b2c54d04b5345e6d47cd833ba814
devoneASC6 <nintendanime02@gmail.com>
2019-08-02 15:09:03 -0400

    here it is

---
+++ script.js
@@ -0,0 +1,7 @@
+function onClick(){
+    
+    document.getElementById(jt).style.color = "red"
+}
+
+let colorChanger = document.querySelector("button")
+colorChanger.addEventListener("click", onClick);
